ee9b17da0e214960744edc48e98726c3ee4cd098
pritampokhra <pritampokhra29@gmail.com>
2025-08-21 23:06:33 +0530

    removed unused imports

--- src/main/java/com/example/demo/config/SpringSecurityConfiguration.java
+++ src/main/java/com/example/demo/config/SpringSecurityConfiguration.java
@@ -8,14 +8,6 @@ import org.springframework.security.web.SecurityFilterChain;
 
 import com.example.demo.service.CustomUserDetailsService;
 
-/*************  ✨ Windsurf Command 🌟  *************/
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 @Configuration
 public class SpringSecurityConfiguration {
 
@@ -34,4 +26,3 @@ public class SpringSecurityConfiguration {
 	}
 
 }
-/*******  b3298e9f-c22e-4054-9edf-817dbe5be89f  *******/
